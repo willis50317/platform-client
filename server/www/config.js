ecab3063fb11c4fc220e754de228bcf0120aa5d9
Robbie Mackay <rm@robbiemackay.com>
2015-08-28 16:25:19 +1200

    Route to API based on current domain + api. #635

--- server/www/config.js
+++ server/www/config.js
@@ -4,3 +4,10 @@
 // window.ushahidi = {
 // 	backendUrl : "https://ushahidi-platform-api-release.herokuapp.com"
 // };
+
+// Cloud API routing
+// Assuming we have a hostname like zombies.ushahidi.io
+// The API will be api.zombies.ushahidi.io
+window.ushahidi {
+	backendUrl : location.protocol + "//api." + location.hostname + (location.port ? ":" + location.port : "");
+};
